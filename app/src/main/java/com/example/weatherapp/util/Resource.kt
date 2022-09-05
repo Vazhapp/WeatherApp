c780277b6066c65806bba25a0eab4855c13c9a49
Vazha Kentchiashvili <vkentchiashvili@gmail.com>
2022-09-05 03:04:12 -0700

    added loading state

--- app/src/main/java/com/example/weatherapp/util/Resource.kt
+++ app/src/main/java/com/example/weatherapp/util/Resource.kt
@@ -6,5 +6,6 @@ package com.example.weatherapp.util
 sealed class Resource<T>(val data: T? = null, val errorMessage: String? = null) {
     class Success<T>(data: T?) : Resource<T>(data)
     class Error<T>(data: T?) : Resource<T>(data)
+    object Loading
 }
 
