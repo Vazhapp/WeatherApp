b3fcb2d001389ce48f6e440fe32c983414dad081
Vazha Kentchiashvili <vkentchiashvili@gmail.com>
2022-09-05 23:50:30 -0700

    Init component

---
+++ app/src/main/java/com/example/weatherapp/di/app/AppComponent.kt
@@ -0,0 +1,10 @@
+package com.example.weatherapp.di.app
+
+import dagger.Component
+
+/**
+ * Created by Vazhapp on 05.09.2022
+ */
+@Component(modules = [AppModule::class])
+interface AppComponent {
+}
